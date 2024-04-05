e0fcfbdc996e0d2ea47e55b4cd4e50acc6267ff5
Ongun <info@ongunakay.com>
2024-04-05 23:32:50 +0300

    modified

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,13 @@
 function addition(a, b) {
-    return a + b;
-  }
-  
-  function addNumbers() {
-    // Get values from input fields
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
-  
-    // Call the addition function and store the result
-    const sum = addition(num1, num2);
-  
-    // Display the result in the paragraph element
-    document.getElementById("result").innerHTML = "The sum is: " + sum;
-  }
-  
+  return a + b;
+}
+
+function addNumbers() {
+
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+
+  const sum = addition(num1, num2);
+
+  document.getElementById("result").innerHTML = "The sum is: " + sum;
+}
